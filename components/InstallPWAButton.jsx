fe6211ba95b4a89f4fb13ad468f6b4fdf0dfc9ae
Ronald <rozu0980@gmail.com>
2025-03-07 09:53:06 +0700

    change; PWA button color

--- components/InstallPWAButton.jsx
+++ components/InstallPWAButton.jsx
@@ -50,7 +50,7 @@ const InstallPWAButton = () => {
       {!isInstalled && deferredPrompt && (
         <button
           onClick={handleInstall}
-          className="px-4 py-2 bg-orange-600 text-white rounded-lg shadow-lg hover:bg-orange-700 transition"
+          className="px-4 py-2 bg-red-600 text-white rounded-lg shadow-lg hover:bg-red-700 transition"
         >
           Install Aplikasi
         </button>
